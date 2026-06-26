2ab09d8cee1a1d401a7df4e842424ef2376954fc
NisCodes <nishad.dhuri06@gmail.com>
2026-06-26 16:01:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,32 @@
 import express from "express";
 import cors from "cors";
-import pg from "pg";
 import env from "dotenv";
 import multer from "multer"; 
+import admin from "firebase-admin";
+
 import { blogRouter } from "./routers/blogRouter.js";
 import { galleryRouter } from "./routers/galleryRouter.js";
 import { messageRouter } from "./routers/messageRouter.js";
 import { poetryRouter } from "./routers/poetryRouter.js";
 import { eventRouter } from "./routers/eventRouter.js";
 import { magazineRouter } from "./routers/magazineRouter.js";
+
 const app = express();
 const port = 4000;
 env.config();
 
-// const db = new pg.Client({
-//   user: process.env.USER,
-//   host: process.env.HOST,
-//   database: process.env.DATABASE,
-//   password: process.env.PASSWORD,
-//   port: process.env.PORT,
-// });
-// db.connect();
+// --- FIREBASE INITIALIZATION ---
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  admin.initializeApp({
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT))
+  });
+} else {
+  admin.initializeApp();
+}
 
-import fs from "fs";
-import url from "url";
-const config = {
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.PORT,
-    database: process.env.DATABASE,
-    ssl: {
-        rejectUnauthorized: true,
-        ca: fs.readFileSync(process.env.CERTIFICATE).toString(),
-    },
-};
-
-const db = new pg.Client(config);
-
-
-db.connect((err) => {
-  if (err) {
-    console.error("Connection error", err.stack);
-  } else {
-    console.log("Connected to the online PostgreSQL database");
-  }
-});
+const db = admin.firestore();
+console.log("Connected to the online Firebase Firestore database");
+// -------------------------------
 
 app.use(cors());
 app.use(express.json());
