70fde29e8b98c812ae5698c9ac876a54790641a8
NisCodes <nishad.dhuri06@gmail.com>
2026-07-01 18:30:00 +0530

    Update karwaanRouter.js

--- backend/routers/karwaanRouter.js
+++ backend/routers/karwaanRouter.js
@@ -1,14 +1,9 @@
 import express from "express";
-import {
-  getKarwaan,
-  deleteKarwaan,
-  pushKarwaan,
-} from "../controllers/karwaanController.js";
+import { getKarwaan, pushKarwaan } from "../controllers/karwaanController.js";
 
 const router = express.Router();
 
 router.post("/post", pushKarwaan);
 router.get("/get", getKarwaan);
-router.delete("/delete/:id", deleteKarwaan);
 
 export { router as karwaanRouter };
