dac9ffb51f9b4ea64b52fe9c1f5a416e9c335873
NisCodes <nishad.dhuri06@gmail.com>
2026-07-05 14:57:51 +0530

    Update newsletterController.js

--- backend/controllers/newsletterController.js
+++ backend/controllers/newsletterController.js
@@ -1,14 +1,7 @@
-import { db } from '../firebaseConfig.js';
+import express from 'express';
+import { getNewsletter } from '../controllers/newsletterController.js';
 
-export const getNewsletter = async (req, res) => {
-  try {
-    const snapshot = await db.collection('newsletter').get();
-    const newsletters = snapshot.docs.map(doc => ({
-      id: doc.id,
-      ...doc.data()
-    }));
-    res.status(200).json(newsletters);
-  } catch (err) {
-    res.status(500).json({ error: "Failed to fetch from Firestore" });
-  }
-};
+const router = express.Router();
+router.get('/get', getNewsletter);
+
+export { router as newsletterRouter };
